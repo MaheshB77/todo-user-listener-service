a95a50e3e06bdc4e14550c1c7146633ab3245858
MaheshB77 <mbansode7696@gmail.com>
2021-04-25 20:39:48 +0530

    Added loggers

--- src/main/java/com/todo/userlistener/listener/UserListener.java
+++ src/main/java/com/todo/userlistener/listener/UserListener.java
@@ -1,6 +1,8 @@
 package com.todo.userlistener.listener;
 
 import com.todo.userlistener.models.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +10,11 @@ import java.util.List;
 
 @Service
 public class UserListener {
+    private static final Logger logger = LogManager.getLogger(UserListener.class);
 
     @KafkaListener(topics = "UserTopic", groupId = "user_group")
     public void consumeUser(List<User> user) {
-        System.out.println("Consumed user : " + user);
+//        System.out.println("Consumed user : " + user);
+        logger.info("User consumed : " + user);
     }
 }
