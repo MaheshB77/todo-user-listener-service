6f11f8ebcd2974d2fdfb6b3f0fbbe3132017780b
MaheshB77 <mbansode7696@gmail.com>
2021-04-26 04:53:18 +0530

    Added comments

--- src/main/java/com/todo/userlistener/listener/UserListener.java
+++ src/main/java/com/todo/userlistener/listener/UserListener.java
@@ -8,13 +8,19 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * @author Mahesh Bansode
+ */
 @Service
 public class UserListener {
     private static final Logger logger = LogManager.getLogger(UserListener.class);
 
+    /**
+     * Listens to the event produced by todo-demo-api
+     * @param user
+     */
     @KafkaListener(topics = "UserTopic", groupId = "user_group")
     public void consumeUser(List<User> user) {
-//        System.out.println("Consumed user : " + user);
         logger.info("User consumed : " + user);
     }
 }
